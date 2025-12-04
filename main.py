38669f316361f08213afb3b765cf2e8f62bf85d4
Daniel Taylor <146564443+dannyjtaylor@users.noreply.github.com>
2025-12-04 14:31:28 -0500

    Test

--- main.py
+++ main.py
@@ -1458,7 +1458,7 @@ async def on_ready():
     await bot.change_presence(
         activity=discord.Activity(
             type=discord.ActivityType.playing,
-            name="running /gather on V0.2.0 :3"
+            name="running /gather on V0.2.1 :3"
         )
     )
     try:
