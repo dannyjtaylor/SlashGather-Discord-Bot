5c51c48c47863c630cbc1da477b567f0f66beb2c
Daniel Taylor <daniel.taylor6456@gmail.com>
2025-12-22 22:44:08 -0500

    Bump version to V0.2.2

--- main.py
+++ main.py
@@ -1591,7 +1591,7 @@ async def on_ready():
     await bot.change_presence(
         activity=discord.Activity(
             type=discord.ActivityType.playing,
-            name="running /gather on V0.2.1 :3"
+            name="running /gather on V0.2.2 :3"
         )
     )
     try:
